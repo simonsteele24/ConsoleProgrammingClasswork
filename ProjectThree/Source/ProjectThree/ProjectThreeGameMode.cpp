42b7ad4f39ee74d87a16084d17247a9f119ba85a
Simon Steele <simon.steele@mymail.champlain.edu>
2021-04-08 14:47:16 -0400

    Added game state

--- ProjectThree/Source/ProjectThree/ProjectThreeGameMode.cpp
+++ ProjectThree/Source/ProjectThree/ProjectThreeGameMode.cpp
@@ -4,6 +4,7 @@
 #include "ProjectThreeHUD.h"
 #include "ProjectThreeCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "GameFramework/GameState.h"
 
 AProjectThreeGameMode::AProjectThreeGameMode()
 	: Super()
@@ -11,6 +12,10 @@ AProjectThreeGameMode::AProjectThreeGameMode()
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// set default game state class to our blueprinted game state instance
+	static ConstructorHelpers::FClassFinder<AGameStateBase> GameStateClassFinder(TEXT("/Game/Blueprints/BP_FPGameState"));
+	GameStateClass = GameStateClassFinder.Class;
 
 	// use our custom HUD class
 	HUDClass = AProjectThreeHUD::StaticClass();

bf8e05afea6a9f2bf61c3999b53151717b442aad
Chun Tao Lin <chuntao.lin@mymail.champlain.edu>
2021-04-27 18:52:45 -0400

    pushing bit by bit

---
+++ ProjectFour/Source/ProjectFour/ProjectFourGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectFourGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTFOUR_API AProjectFourGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
